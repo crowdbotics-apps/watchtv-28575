35f29e92592e51869603b1e64dd23d458d1ff237
Crowdbotics <team@crowdbotics.com>
2021-07-08 02:10:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/watchtv_28575/MainActivity.java
@@ -0,0 +1,15 @@
+package com.watchtv_28575;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "watchtv_28575";
+  }
+}
